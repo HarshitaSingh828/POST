38ba9402dc0f20e0a052fdaf77d20a5e75bb721b
Harshita Singh <singhharshita@828gmail.com>
2023-06-03 10:26:26 +0530

    p

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(
 );
 dotEnv.config({ path: "./.env" });
 const port = process.env.PORT || 5000;
+
+
 mongoose
   .connect(process.env.MONGO_DB_URL,{useNewUrlParser:true, useUnifiedTopology:true})
   .then(() => console.log("DB Connected"))
